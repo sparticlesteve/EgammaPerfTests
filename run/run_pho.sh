4be9689bc731b8f2728e8deeef8da2e8f170723f
Steve Farrell <sfarrell@cern.ch>
2015-01-25 16:42:38 -0800

    added log file

--- run/run_pho.sh
+++ run/run_pho.sh
@@ -2,6 +2,6 @@
 
 # Run the photon job
 date
-valgrind --tool=callgrind --cache-sim=yes --instr-atstart=no photonCalibPerf ./xAOD.pool.root 50
+valgrind --tool=callgrind --cache-sim=yes --instr-atstart=no photonCalibPerf ./xAOD.pool.root 50 2>&1 | tee pho.log
 echo "finished"
 date
